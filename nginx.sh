ccc24506d45ddca37993551af25e60f9ff4a4cd4
Alex Kova <kova76@mail.ru>
2020-02-01 12:20:31 +0000

    2020-02-01 v3

--- nginx.sh
+++ nginx.sh
@@ -3,7 +3,8 @@
 echo "Nginx installation"
 sudo apt install nginx -y
 echo "Nginx settings"
-sudo cp ~/luravele/nginx/nginx.conf /etc/nginx/nginx.conf
-sudo cp ~/luravele/nginx/default /etc/nginx/sites-available/default
+sudo cp /usr/lib/luravele/nginx/nginx.conf /etc/nginx/nginx.conf
+sudo cp /usr/lib/luravele/nginx/default /etc/nginx/sites-available/default
+service nginx restart
 
 exit 0
